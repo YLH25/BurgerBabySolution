d6068ce13ddea704a5f2942e303e67099978558c
YLH25 <1402howard@gmail.com>
2024-12-24 19:32:02 +0800

    修正新增會員不必要的綁定

--- BurgerBaby/Controllers/MemberController.cs
+++ BurgerBaby/Controllers/MemberController.cs
@@ -92,7 +92,7 @@ namespace BurgerBaby.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,RoleId,Phone,Address,Email,Password,Name")] MemberCreateVM memberCreateVM)
+        public async Task<IActionResult> Create([Bind("RoleId,Phone,Address,Email,Password,Name")] MemberCreateVM memberCreateVM)
         {
             try
             {
