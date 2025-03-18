290c304d2d2488ee99c57462012d7527e4ef9963
LAPTOP-AT9EP12R\Yang <1402howard@gmail.com>
2025-03-18 14:53:07 +0800

    刪減測試用代碼

--- BurgerBabyApi/Program.cs
+++ BurgerBabyApi/Program.cs
@@ -61,43 +61,6 @@ namespace BurgerBabyApi
                     )
                 );
 
-           
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "Your API",
-                    Version = "v1",
-                    Description = "API with JWT Authentication"
-                });
-
-                
-                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.Http,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Description = "Enter 'Bearer' [space] and then your token in the text input below.\n\nExample: \"Bearer abc123xyz\""
-                });
-
-                options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
-            });
-
             var app = builder.Build();
             app.UseCors("MemberPolicy");
 
